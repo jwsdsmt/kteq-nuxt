b8415fb5294c18fdbc80ef3eea46b3e6343ed7cd
jwsdsmt <jason.ward@sdsmt.edu>
2025-05-11 22:25:05 -0600

    Update proxyStream.js
    Link stream, not playlist

--- netlify/functions/proxyStream.js
+++ netlify/functions/proxyStream.js
@@ -1,17 +1,16 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  const streamUrl = 'http://kteq-streamer.sdsmt.edu:8000/kteq_high.m3u';
-
+  const streamUrl = 'http://kteq-streamer.sdsmt.edu:8000/kteq_high';
   const res = await fetch(streamUrl);
-  const body = await res.text();
 
   return {
     statusCode: 200,
     headers: {
-      'content-type': 'audio/x-mpegurl',
+      'content-type': 'audio/mpeg',
       'access-control-allow-origin': '*',
     },
-    body,
+    body: res.body,
+    isBase64Encoded: false,
   };
 };
