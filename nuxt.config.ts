364206ecfeb1e8077203b392d7cca4204a6ca948
jwsdsmt <jason.ward@sdsmt.edu>
2025-05-11 21:20:45 -0600

    Prepare for Netlify deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: '/kteq-nuxt/'
+    baseURL: '/'
   },
   css: ['public/assets/css/style.css'],
-  vite:{
-    plugins: [
-      tsconfigPaths()
-    ],
+  vite: {
+    plugins: [tsconfigPaths()]
   },
   devtools: { enabled: true },
   compatibilityDate: '2024-11-01',
